327d3fab55d1214f3a63d339f1f5d4405b6ea091
thiên lê <thienle255@gmail.com>
2024-01-19 00:46:29 +0700

    Update CoursesService.cs

--- backend-app/Services/dashboard/CoursesService.cs
+++ backend-app/Services/dashboard/CoursesService.cs
@@ -29,13 +29,10 @@ namespace backend_app.Services.dashboard
         {
             string str = phrase.ToLowerInvariant();
 
-            // invalid chars           
             str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
-            // convert multiple spaces into one space   
             str = Regex.Replace(str, @"\s+", " ").Trim();
-            // cut and trim 
             str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
-            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            str = Regex.Replace(str, @"\s", "-");   
 
             return str;
         }
