afa336af977ed3d865fdf358cf271639be3cb51b
ngmquann <117283361+ngmquann@users.noreply.github.com>
2024-01-18 23:50:33 +0700

    a

--- backend-app/DTO/Account.cs
+++ backend-app/DTO/Account.cs
@@ -3,8 +3,6 @@
     public class Account
     {
         public int Id { get; set; }
-
-
         public string Email { get; set; }
         public string Name { get; set; }
         public string Role { get; set; }
