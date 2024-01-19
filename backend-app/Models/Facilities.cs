a90eab78d8a324f8ed1b6d41391d07feb28902b4
ThanhChuong1 <thanhchuong1412@gmail.com>
2024-01-19 20:58:31 +0700

    a

---
+++ backend-app/Models/Facilities.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend_app.Models
+{
+    public class Facilities
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+       
+    }
+}
