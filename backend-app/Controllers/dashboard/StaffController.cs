66e220f2619a0319e97f3328afff86fa619cf6e8
ngmquann <117283361+ngmquann@users.noreply.github.com>
2024-01-19 22:04:14 +0700

    Staff

---
+++ backend-app/Controllers/dashboard/StaffController.cs
@@ -0,0 +1,48 @@
+﻿using backend_app.IRepository.dashboard;
+using backend_app.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_app.Controllers.dashboard
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private IStaff service;
+        public StaffController(IStaff service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Staff> GetOneStaff(int id)
+        {
+            return await service.GetOneStaff(id);
+        }
+
+        [HttpGet("GetList")]
+        public async Task<IEnumerable<Staff>> GetList()
+        {
+            return await service.GetAllStaffs();
+        }
+
+        [HttpPost]
+        public async Task<Staff> PostCourse(Staff staff)
+        {
+            return await service.AddStaff(staff);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<Staff> DeleteStaff(int id)
+        {
+            return await service.DeleteStaff(id);
+        }
+
+        [HttpPut]
+        public async Task<Staff> PutStaff(Staff staff)
+        {
+            return await service.UpdateStaff(staff);
+        }
+    }
+}
---
+++ backend-app/Models/Staff.cs
@@ -0,0 +1,18 @@
+﻿namespace backend_app.Models
+{
+    public class Staff
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public bool Gender { get; set; }
+        public string Phone { get; set; }
+        public string FileAvatar { get; set; }
+        public string Qualification { get; set; }
+        public string Experience { get; set; }
+        public string Role { get; set; }
+        public string Password { get; set; }
+    }
+}
